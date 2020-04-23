5ffeedce4053d86b9e0505a4f48515ad8d320e75
Jonathan Pentecost <jonathan.pentecost@points.com>
2020-04-23 09:00:38 -0400

    mdns working for simulator

--- cmd/simulator/main.go
+++ cmd/simulator/main.go
@@ -11,9 +11,15 @@ import (
 const (
 	mdnsAddr             = "224.0.0.251:5353"
 	chromecastLookupName = "_googlecast._tcp.local."
+	simulatorType        = "ChromecastSimulator"
+	simulatorName        = "chromcast-simulator-test"
+	simulatorID          = "abcdef0123456789"
+	// Google-Home-Mini-b87d86bed423a6feb8b91a7d2778b55c._googlecast._tcp.local.
+	simulatorDNS = simulatorType + "-" + simulatorID + "." + chromecastLookupName
 )
 
 func main() {
+	fmt.Printf("serving %q...\n", simulatorDNS)
 	ifaces, err := net.Interfaces()
 	if err != nil {
 		log.Fatal(err)
@@ -93,6 +99,9 @@ func startMDNSServer(ip string, port int) error {
 	if err != nil {
 		return err
 	}
+
+	ipv4IP := net.ParseIP(ip)
+
 	log.Printf("listening for mdns on %s\n", mdnsAddr)
 	go func() {
 		defer pc.Close()
@@ -109,12 +118,11 @@ func startMDNSServer(ip string, port int) error {
 				log.Printf("error: unable to unpack packet: %v", err)
 				continue
 			}
+
 			for _, q := range msg.Question {
 				if q.Name != chromecastLookupName {
 					continue
 				}
-				fmt.Printf("dns msg: %#v\n", msg)
-				fmt.Printf("dns msg question: %#v\n", q)
 
 				resp := dns.Msg{}
 				resp.SetReply(&msg)
@@ -123,21 +131,23 @@ func startMDNSServer(ip string, port int) error {
 				resp.Authoritative = true
 				resp.Question = nil // RFC6762 section 6 "responses MUST NOT contain any questions"
 
+				// MSG answers: 0) &dns.PTR{Hdr:dns.RR_Header{Name:"_googlecast._tcp.local.", Rrtype:0xc, Class:0x1, Ttl:0x78, Rdlength:0x33}, Ptr:"Nest-Wifi-point-8874243a15830f7844419710c67933f5._googlecast._tcp.local."}
 				ptr := &dns.PTR{
 					Hdr: dns.RR_Header{
-						Name:   chromecastLookupName,
+						Name:   q.Name,
 						Rrtype: dns.TypePTR,
 						Class:  dns.ClassINET,
 						Ttl:    120,
 					},
-					Ptr: "chromecast-simulator",
+					Ptr: simulatorDNS,
 				}
 
 				resp.Answer = []dns.RR{ptr}
 
+				// MSG extras: 1) &dns.SRV{Hdr:dns.RR_Header{Name:"Nest-Wifi-point-8874243a15830f7844419710c67933f5._googlecast._tcp.local.", Rrtype:0x21, Class:0x8001, Ttl:0x78, Rdlength:0x2d}, Priority:0x0, Weight:0x0, Port:0x1f49, Target:"8874243a-1583-0f78-4441-9710c67933f5.local."}
 				srv := &dns.SRV{
 					Hdr: dns.RR_Header{
-						Name:   chromecastLookupName,
+						Name:   simulatorDNS,
 						Rrtype: dns.TypeSRV,
 						Class:  dns.ClassINET,
 						Ttl:    120,
@@ -145,20 +155,37 @@ func startMDNSServer(ip string, port int) error {
 					Priority: 0,
 					Weight:   0,
 					Port:     uint16(port),
-					Target:   ip,
+					Target:   simulatorID + ".local.",
 				}
 
+				// MSG extras: 2) &dns.A{Hdr:dns.RR_Header{Name:"8874243a-1583-0f78-4441-9710c67933f5.local.", Rrtype:0x1, Class:0x8001, Ttl:0x78, Rdlength:0x4}, A:net.IP{0xc0, 0xa8, 0x56, 0x1c}}<Paste>
 				a := &dns.A{
 					Hdr: dns.RR_Header{
-						Name:   "chromecast-simulator",
+						Name:   simulatorID + ".local.",
 						Rrtype: dns.TypeA,
 						Class:  dns.ClassINET,
 						Ttl:    120,
 					},
-					A: net.IP{192, 168, 12, 13},
+					A: ipv4IP,
+				}
+				/*
+				   MSG extras: 0) &dns.TXT{Hdr:dns.RR_Header{Name:"Nest-Wifi-point-8874243a15830f7844419710c67933f5._googlecast._tcp.local.", Rrtype:0x10, Class:0x8001, Ttl:0x1194, Rdlength:0xc2}, Txt:[]string{"id=8874243a15830f7844419710c67933f5", "cd=B20EF1BF3BE2BD2AAD9C5596F282F14E", "rm=CB187BA191653173", "ve=05", "md=Nest Wifi point", "ic=/setup/icon.png", "fn=Bedroom Wifi 2", "ca=198660", "st=0", "bs=FA8FCA5E8DE9", "nf=1", "rs="}}
+				*/
+				txt := &dns.TXT{
+					Hdr: dns.RR_Header{
+						Name:   simulatorDNS,
+						Rrtype: dns.TypeTXT,
+						Class:  dns.ClassINET,
+						Ttl:    120,
+					},
+					Txt: []string{
+						"id=" + simulatorID,
+						"md=" + simulatorType,
+						"fn=" + simulatorName,
+					},
 				}
 
-				resp.Extra = []dns.RR{srv, a}
+				resp.Extra = []dns.RR{srv, a, txt}
 
 				packedAnswer, err := resp.Pack()
 				if err != nil {
