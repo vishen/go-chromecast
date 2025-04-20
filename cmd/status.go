fd270c6d419badd3269b3eeb1d273b119a248072
Oleksii Shurubura <kxalex@gmail.com>
2025-04-19 23:57:19 -0500

    feat: Enhance status command output with content ID option
    - Added a flag to the status command to optionally print the content ID if available.
    - Refactored output logic for improved readability and maintainability.
    - Updated github actions and goreleaser configuration

--- cmd/status.go
+++ cmd/status.go
@@ -30,28 +30,57 @@ var statusCmd = &cobra.Command{
 			exit("unable to get cast application: %v", err)
 		}
 		castApplication, castMedia, castVolume := app.Status()
-		if castApplication == nil {
-			outputInfo("Idle, volume=%0.2f muted=%t", castVolume.Level, castVolume.Muted)
-		} else if castApplication.IsIdleScreen {
-			outputInfo("Idle (%s), volume=%0.2f muted=%t", castApplication.DisplayName, castVolume.Level, castVolume.Muted)
-		} else if castMedia == nil {
-			outputInfo("Idle (%s), volume=%0.2f muted=%t", castApplication.DisplayName, castVolume.Level, castVolume.Muted)
+		volumeLevel := castVolume.Level
+		volumeMuted := castVolume.Muted
+
+		contentId, _ := cmd.Flags().GetBool("content-id")
+
+		scriptMode := contentId
+
+		if scriptMode {
+			if contentId {
+				if castMedia != nil {
+					outputInfo(castMedia.Media.ContentId)
+				} else {
+					outputInfo("not available")
+				}
+			}
+		} else if castApplication == nil {
+			outputInfo("Idle, volume=%0.2f muted=%t", volumeLevel, volumeMuted)
 		} else {
-			metadata := "unknown"
-			var usefulID string
-			switch castMedia.Media.ContentType {
-			case "x-youtube/video":
-				usefulID = fmt.Sprintf("[%s] ", castMedia.Media.ContentId)
+			displayName := castApplication.DisplayName
+			if castApplication.IsIdleScreen {
+				outputInfo("Idle (%s), volume=%0.2f muted=%t", displayName, volumeLevel, volumeMuted)
+			} else if castMedia == nil {
+				outputInfo("Idle (%s), volume=%0.2f muted=%t", displayName, volumeLevel, volumeMuted)
+			} else {
+				var metadata string
+				var usefulID string
+				switch castMedia.Media.ContentType {
+				case "x-youtube/video":
+					usefulID = fmt.Sprintf("[%s] ", castMedia.Media.ContentId)
+				}
+				if castMedia.Media.Metadata.Title != "" {
+					md := castMedia.Media.Metadata
+					metadata = fmt.Sprintf("title=%q, artist=%q", md.Title, md.Artist)
+				}
+				if castMedia.Media.ContentId != "" {
+					if metadata != "" {
+						metadata += ", "
+					}
+					metadata += fmt.Sprintf("[%s]", castMedia.Media.ContentId)
+				}
+				if metadata == "" {
+					metadata = "unknown"
+
+				}
+				outputInfo("%s%s (%s), %s, time remaining=%.0fs/%.0fs, volume=%0.2f, muted=%t", usefulID, displayName, castMedia.PlayerState, metadata, castMedia.CurrentTime, castMedia.Media.Duration, volumeLevel, volumeMuted)
 			}
-			if castMedia.Media.Metadata.Title != "" {
-				md := castMedia.Media.Metadata
-				metadata = fmt.Sprintf("title=%q, artist=%q", md.Title, md.Artist)
-			}
-			outputInfo("%s%s (%s), %s, time remaining=%.0fs/%.0fs, volume=%0.2f, muted=%t", usefulID, castApplication.DisplayName, castMedia.PlayerState, metadata, castMedia.CurrentTime, castMedia.Media.Duration, castVolume.Level, castVolume.Muted)
 		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(statusCmd)
+	statusCmd.Flags().Bool("content-id", false, "print the content id if available")
 }
